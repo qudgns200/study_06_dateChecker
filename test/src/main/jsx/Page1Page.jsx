18daa7ffa65dd3693a5842be731bb8c1c3ae8516
unknown <qudgns200@gmail.com>
2020-01-20 15:42:07 +0900

    Grommet Practice

--- test/src/main/jsx/Page1Page.jsx
+++ test/src/main/jsx/Page1Page.jsx
@@ -1,16 +1,34 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-import { Button } from "grommet";
+import { WorldMap } from "grommet";
 
 class Page1Page extends React.Component {
   render() {
     return (
-      <header className="page1">
-        <Button label="Page1" onClick={() => {}} />
+      <header className="page2">
+        <WorldMap
+          color="neutral-1"
+          continents={[
+            {
+              name: "Africa",
+              color: "light-5",
+              onClick: name => {}
+            }
+          ]}
+          onSelectPlace={(lat, lon) => {}}
+          places={[
+            {
+              name: "Sydney",
+              location: [-33.8830555556, 151.216666667],
+              color: "accent-2",
+              onClick: name => {}
+            }
+          ]}
+          selectColor="accent-2"
+        />
       </header>
     );
-    //<div className="page1">no4gift Page1 페이지</div>;
   }
 }
 
